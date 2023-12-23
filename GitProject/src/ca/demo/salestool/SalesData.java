14f9cf542c1a5b70111271ecaa524543a627a225
RosaMo <rosaatcoding@gmail.com>
2023-12-23 23:35:53 +0100

    Lucy added msg

--- GitProject/src/ca/demo/salestool/SalesData.java
+++ GitProject/src/ca/demo/salestool/SalesData.java
@@ -1,12 +1,11 @@
 package ca.demo.salestool;
 
 public class SalesData {
-	
-	private int[] data = {0, 4, 42};
 
 	public void display() {
 		System.out.println("It worked! You are a genius");
 		System.out.println("Yes, I am!");
+		System.out.println("holitis:)");
 	}
 	
 }
