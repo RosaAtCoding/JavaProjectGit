9fc8ebd47fecd24cbd517a0f7bb6a0cf8283c124
RosaMo <rosaatcoding@gmail.com>
2023-12-23 12:36:05 +0100

    First commit after init

--- GitProject/src/ca/demo/salestool/SalesData.java
+++ GitProject/src/ca/demo/salestool/SalesData.java
@@ -5,7 +5,7 @@ public class SalesData {
 	private int[] data = {0, 4, 42};
 
 	public void display() {
-		System.out.println("TODO: Implement data display");
+		System.out.println("It worked!");
 	}
 	
 }
